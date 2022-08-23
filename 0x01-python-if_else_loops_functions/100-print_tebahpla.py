1356efb596e681274115abf9d2b6c823d85b8cfc
Vincejohn <vincejohn987@gmail.com>
2022-08-23 19:08:45 +0000

    Advanced correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author -Vimcent John
+# Author -Vincent John
 
 i = 0
-for c in range(ord('z'), ord('a') -1, -1):
+for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
